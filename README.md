031cd39f6b997186fe06516e572d28396c5e070a
Manuel Herrera <manuel.he@gmail.com>
2015-10-22 12:09:04 -0500

    Added tabs navigation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bogotá Mayor 2015 Poll
 
 Just a simple application to show the results with graphs.
+
+## Rants
+
+I know. Everything in just one file? That's awful, but who cares. A PHP class, the HTML template, the Javascript application, the styles. Oh my! Someone will hit me in the head for this thing.
--- index.php
+++ index.php
@@ -118,9 +118,10 @@ $parsedData = new \DataParse\FilterData();
     <title>Resultados Encuesta Voto Alcaldía de Bogotá 2015</title>
     <meta name="viewport" content="width=device-width">
     <style>
-    *,*:before,*:after{box-sizing:border-box;}html{font-size: 100%;}body{font: 16px/1.4 Helvetica, Arial, sans-serif;margin:0;padding:0;background:#c8d3d9}
+    *,*:before,*:after{box-sizing:border-box;}html{font-size: 100%;}body{font: 16px/1.4 Helvetica, Arial, sans-serif;margin:0;padding:0;background:#c8d3d9}h1,h2,h3,h4{margin-top:0}
     .row{display:table;width:100%}.row:before,.row:after{content:" ";display:table}.column{float:left;padding-left:.83333rem;padding-right:.83333rem;width:100%}.colspan-1{width:8.33333%}.colspan-2{width:16.66667%}.colspan-3{width:25%}.colspan-4{width:33.33333%}.colspan-5{width:41.66667%}.colspan-6{width:50%}.colspan-7{width:58.33333%}.colspan-8{width:66.66667%}.colspan-9{width:75%}.colspan-10{width:83.33333%}.colspan-11{width:91.66667%}.colspan-12{width:100%}
       .container{width:80rem;margin:0 auto;padding:2rem 1rem;border-left:1px solid #ccc;border-left:1px solid #bbb;background:#fff}
+      .navigation ul{margin:2rem 0 0;padding:0;list-style:none;background:#fff}.navigation li{display:inline-block;cursor:pointer;padding:10px 30px;color:#375DD8}.navigation li.active,.navigation li:hover{border-bottom:5px solid #FF8436}.navigation li.active{color:#789}.tab_content{border:1px solid #d5d6d9;display:none;padding:1em}.tab_content.active{display:block}
     </style>
 
     <!--Load the AJAX API-->
@@ -130,7 +131,7 @@ $parsedData = new \DataParse\FilterData();
 
       var basicDataSets = {
         age: {
-          title: "Rangos de edad",
+          title: "Rangos de edad en años",
           columns: ['Edad','Total'],
           data: <?php echo $parsedData->getGoogleGraphData(2);?>
         },
@@ -255,6 +256,7 @@ $parsedData = new \DataParse\FilterData();
           }
           return obj;
       };
+
     </script>
   </head>
 
@@ -267,28 +269,71 @@ $parsedData = new \DataParse\FilterData();
         Esta fue una encuesta informal sin ninguna base sólida que no intentó determinar nada, igual que las que hacen los grandes encuestadores del mundo mundial. Esta fue una encuesta anónima no patrocinada por ninguna entidad, partido o candidato.
       </p>
       <p>Los siguiente resultados no pueden ser tomados más que por un corto y tartamudo vaticinio.</p>
-      <p>La tabla de datos puede descargarse desde el siguiente enlace: <a href="./data.csv" download="ResultadosVotoBogota2015.csv">Resultados Voto Bogota 2015</a></p>
-      <div class="row">
-        <div id="chart1_div" class="column colspan-6"></div>
-        <div id="chart2_div" class="column colspan-6"></div>
+      <p>La tabla de datos puede descargarse desde el siguiente enlace: <a href="./data/data.csv" download="ResultadosVotoBogota2015.csv">Resultados Voto Bogota 2015</a></p>
+
+      <div class="navigation">
+        <ul>
+          <li class="active">Resultados Generales</li>
+          <li class="">Resultados Correlacionados</li>
+          <li class="">Algunas Conclusiones</li>
+        </ul>
       </div>
-      <div class="row">
-        <div id="chart3_div" class="column colspan-6"></div>
-        <div id="chart4_div" class="column colspan-6"></div>
+
+      <div id="content_tabs">
+
+        <div class="tab_content active">
+          <div class="row">
+            <div id="chart1_div" class="column colspan-6"></div>
+            <div id="chart2_div" class="column colspan-6"></div>
+          </div>
+          <div class="row">
+            <div id="chart3_div" class="column colspan-6"></div>
+            <div id="chart4_div" class="column colspan-6"></div>
+          </div>
+          <div class="row">
+            <div id="chart5_div" class="column colspan-6"></div>
+            <div id="chart6_div" class="column colspan-6"></div>
+          </div>
+          <div class="row">
+            <div id="chart7_div" class="column colspan-6"></div>
+            <div id="chart8_div" class="column colspan-6"></div>
+          </div>
+          <div class="row">
+            <div id="chart9_div" class="column colspan-6"></div>
+            <div id="chart10_div" class="column colspan-6"></div>
+          </div>
+        </div>
+
+        <div class="tab_content">
+          <div id="chart11_div"></div>
+        </div>
+
+        <div class="tab_content">
+          <h2>Algunas conclusiones obias y otras no tanto</h2>
+          <ul>
+            <li>Tengo muy pocos amigos y soy un pésimo community manager.</li>
+            <li>O existe una gran colonia rusa ortodoxa en Colombia o mucho no saben ni a que religión pertenecen.</li>
+            <li>Son los jóvenes los que más participan en redes sociales, pero los viejs son (somos) los que elegimos al final y como siempre.</li>
+          </ul>
+        </div>
+
       </div>
-      <div class="row">
-        <div id="chart5_div" class="column colspan-6"></div>
-        <div id="chart6_div" class="column colspan-6"></div>
-      </div>
-      <div class="row">
-        <div id="chart7_div" class="column colspan-6"></div>
-        <div id="chart8_div" class="column colspan-6"></div>
-      </div>
-      <div class="row">
-        <div id="chart9_div" class="column colspan-6"></div>
-        <div id="chart10_div" class="column colspan-6"></div>
-      </div>
-      <div id="chart11_div"></div>
+
     </div>
   </body>
+  <script>
+    var tabLinks = document.querySelectorAll('.navigation li');
+    var tabContent = document.querySelectorAll('#content_tabs .tab_content');
+    Array.prototype.forEach.call(tabLinks, function(el, i){
+      el.addEventListener('click', function(event) {
+        Array.prototype.forEach.call(tabLinks, function(el2){
+          el2.className = (el === el2) ? 'active' : '';
+        });
+        Array.prototype.forEach.call(tabContent, function(el2, i2){
+          el2.className = (i === i2) ? 'tab_content active' : 'tab_content';
+        });
+      });
+    });
+  </script>
+
 </html>
