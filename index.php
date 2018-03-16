77ff064a2258a86f733278ba6087d4023d763e97
Manuel Herrera <manuel.he@gmail.com>
2018-03-16 11:18:31 -0500

    Added more config settings. Updated GA script

--- index.php
+++ index.php
@@ -140,12 +140,12 @@ $config = parse_ini_file($baseDir.'config/config.ini');
     <meta name="viewport" content="width=device-width">
     <meta property="og:title" content="<?php echo $config['siteTitle'];?>">
     <meta property="og:type" content="article">
-    <meta property="og:site_name" content="Voto Bogotá 2015">
+    <meta property="og:site_name" content="<?php echo $config['smSiteName'];?>">
     <meta property="og:url" content="<?php echo $baseUrl?>">
     <meta property="og:image" content="<?php echo $baseUrl?>/res/fb_thumb.png">
     <meta property="og:image:width" content="1300">
     <meta property="og:image:height" content="780">
-    <meta property="og:description" content="Esta fue una encuesta informal sin ninguna base sólida que no intentó determinar nada, igual que las que hacen los grandes encuestadores del mundo mundial. Esta fue una encuesta anónima no patrocinada por ninguna entidad, partido o candidato.">
+    <meta property="og:description" content="<?php echo $config['smDescription'];?>">
     <meta name="twitter:card" content="player">
     <meta name="twitter:title" content="<?php echo $config['siteTitle'];?>">
     <meta name="twitter:url" content="<?php echo $baseUrl?>">
@@ -153,8 +153,9 @@ $config = parse_ini_file($baseDir.'config/config.ini');
     <meta name="twitter:player:width" content="435">
     <meta name="twitter:player:height" content="261">
     <meta name="twitter:player" content="<?php echo $baseUrl?>">
-    <meta name="twitter:description" content="Esta fue una encuesta informal sin ninguna base sólida que no intentó determinar nada, igual que las que hacen los grandes encuestadores del mundo mundial. Esta fue una encuesta anónima no patrocinada por ninguna entidad, partido o candidato.">
-    <meta name="twitter:site" content="@fractalsoftware">
+    <meta name="twitter:description" content="<?php echo $config['smDescription'];?>">
+    <meta name="twitter:site" content="<?php echo $config['twitter']['site'];?>">
+    <meta name="twitter:creator" content="<?php echo $config['twitter']['creator'];?>">
 
     <style>
     *,*:before,*:after{box-sizing:border-box;}html{font-size: 100%;}body{font: 16px/1.4 Helvetica, Arial, sans-serif;margin:0;padding:0;background:#c8d3d9}h1,h2,h3,h4{margin-top:0}
@@ -346,15 +347,13 @@ $config = parse_ini_file($baseDir.'config/config.ini');
       };
 
     </script>
-    <script type="text/javascript">
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-39323096-1']);
-          _gaq.push(['_trackPageview']);
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $config['gAnalyticsId'];?>"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '<?php echo $config['gAnalyticsId'];?>');
     </script>
   </head>
 
