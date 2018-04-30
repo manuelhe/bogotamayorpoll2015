9d5fe33cdf6ddf0e3b1f153ae3a198b5cb20e52d
Manuel Herrera <manuel.he@gmail.com>
2018-04-29 22:28:53 -0500

    Added selected filters property

--- app/src/DataParse/FilterFields.php
+++ app/src/DataParse/FilterFields.php
@@ -6,11 +6,23 @@ class FilterFields
   private $db;
   private $fields = [];
   private $filterParams = [];
+  private $selectedFilters = [];
 
-  public function __construct($config, $filterParams = [])
+  public function __construct($config, Array $filterParams = [], \DataParse\GetData $parsedData)
   {
     $this->db = $config['db'];
     $this->filterParams = $filterParams;
+
+    if (isset($filterParams['date_init']) && isset($filterParams['date_end']) && (
+        $filterParams['date_init'] !== $parsedData->getMinDate() ||
+        $filterParams['date_end'] !== $parsedData->getMaxDate())) {
+      $this->selectedFilters[] = [
+        'id' => 'date',
+        'label' => "{$filterParams['date_init']} ⇾ {$filterParams['date_end']}",
+        'filter' => 'date_range'
+      ];
+    }
+
     $this->getEnumFields();
     $this->getRelatedFieldsFor('candidate');
     $this->getRelatedFieldsFor('location');
@@ -27,10 +39,18 @@ class FilterFields
         preg_match("/^enum\(\'(.*)\'\)$/", $d['Type'], $matches);
         $matches = explode("','", $matches[1]);
         $this->fields[$d['Field']] = array_map(function ($item) use ($d) {
+          $isSelected = $this->isOptionSelected($item, $d['Field']);
+          if ($isSelected) {
+            $this->selectedFilters[] = [
+              'id' => $item,
+              'label' => $item,
+              'filter' => $d['Field']
+            ];
+          }
           return [
             'id'        => $item, 
             'value'     => $item,
-            'selected'  => $this->isOptionSelected($item, $d['Field'])
+            'selected'  => $isSelected
           ];
         }, $matches);
       }
@@ -49,10 +69,18 @@ class FilterFields
       $values = [];
       $sa = $this->db->query("SELECT * FROM {$tableName}");
       while($d = $sa->fetch(\PDO::FETCH_ASSOC)){
+        $isSelected = $this->isOptionSelected($d[$idFieldName], $tableName);
+        if ($isSelected) {
+          $this->selectedFilters[] = [
+            'id' => $d[$idFieldName],
+            'label' => $d['name'],
+            'filter' => $tableName
+          ];
+        }
         $values[] = [
           'id'        => $d[$idFieldName], 
           'value'     => $d['name'],
-          'selected'  => $this->isOptionSelected($d[$idFieldName], $tableName)
+          'selected'  => $isSelected
         ];
       }
       $this->fields[$tableName] = $values;
@@ -89,6 +117,15 @@ class FilterFields
       return [];
     }
     return $this->fields[$field];
+  }
+
+  /**
+   * Returns selected filters, obviously
+   * 
+   * @return array
+   */
+  public function getSelectedFilters() {
+    return $this->selectedFilters;
   }
 
 }
